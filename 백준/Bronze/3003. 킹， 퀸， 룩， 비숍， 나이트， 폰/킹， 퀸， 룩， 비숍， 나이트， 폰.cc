f23f1e5c153b07a0ee96f14fcecd6ef748f2fea6
Jihyun Lee <91349474+ji-hyun219@users.noreply.github.com>
2023-01-30 22:50:38 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a[6];
+    int b[6] = {1, 1, 2, 2, 2, 8};
+    for (int i=0; i<6; i++){
+        cin >> a[i];
+    }
+    for (int i=0; i<6; i++){
+        cout << b[i] - a[i] << " ";
+    }
+    return 0;  
+}
