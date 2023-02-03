4a4ff6862a8c62f08297667ee4ec5144a2c88179
Jihyun Lee <91349474+ji-hyun219@users.noreply.github.com>
2023-02-03 23:20:11 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a,b;
+  cin >> a;
+  cin >> b;
+  if (a > 0 && b > 0) cout << "1";
+  if (a < 0 && b > 0) cout << "2";
+  if (a < 0 && b < 0) cout << "3";
+  if (a > 0 && b < 0) cout << "4";
+  return 0;
+}
