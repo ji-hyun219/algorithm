4f39ede9973d7c095fe58d0022155f89d1d4a9bc
Jihyun Lee <91349474+ji-hyun219@users.noreply.github.com>
2023-02-08 23:14:27 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n, a, b;
+    cin >> n;
+    for (int i=0; i<n; i++){
+        cin >> a >> b;
+        cout << a+b << "\n";
+        }
+        return 0;
+}
