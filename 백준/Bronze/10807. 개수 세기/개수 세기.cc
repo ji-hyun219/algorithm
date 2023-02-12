e1d4c11338d7c00ed624f2b4dd27f5b2f20d623b
Jihyun Lee <91349474+ji-hyun219@users.noreply.github.com>
2023-02-12 21:48:07 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL);
-    int n, t, answer;
-    int v=0;
-    cin >> n;
-    vector<int> arr(n);
-    for(int i=0; i<n; i++){
-      cin >> t;
-      arr[i] = t;
-        }
-    cin >> answer;
-    for(int j=0; j<n; j++){
-      if (answer == arr[j]) v++; 
-        }
-        cout << v;
-        return 0;
+int main() {
+	int N;
+	cin >> N;
+	int* a = new int[N];
+	for (int i = 0; i < N; i++) {
+		cin >> a[i];
+	}
+	int v;
+	cin >> v;
+	int count = 0;
+	for (int i = 0; i < N; i++) {
+		if (a[i] == v) {
+			count++;
+		}
+	}
+	cout << count;
     
+    delete a;
 }
